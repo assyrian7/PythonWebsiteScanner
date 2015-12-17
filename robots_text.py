9dd3d8b8ed3b5b13fd7956f31c952461121095ec
Paul Badalian <bad.alien7@gmail.com>
2015-12-17 02:30:52 -0500

    Realized how much of a failure this program is

--- robots_text.py
+++ robots_text.py
@@ -1,14 +1,20 @@
-from urllib.request import *
+from urllib.request import urlopen
 import io
 
 def get_robots_txt(url):
-    if url.endswith('/'):
-        path = url
-    else:
-        path = url + '/'
-    req = urlopen(path + "robots.txt", data=None)
-    data = io.TextIOWrapper(req, encoding='utf-8')
-    return data.read()
+        if url.endswith('/'):
+                path = url
+        else:
+                path = url + '/'
+        try:
+                
+                req = urlopen(path + "robots.txt", data=None)
+                data = io.TextIOWrapper(req, encoding='utf-8')
+                return data.read()
+
+        except Exception as e:
+                return "Recalling robots_txt failed"
+
 
 
 
